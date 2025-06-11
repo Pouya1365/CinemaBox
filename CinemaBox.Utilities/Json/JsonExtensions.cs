70d41b3181a19c91833618000a2c2a92eb5a028b
Pouya <Pouya@DESKTOP-N60CR32>
2025-06-11 20:13:28 +0330

    json

---
+++ CinemaBox.Utilities/Json/JsonExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+
+namespace CinemaBox.Utilities.Json;
+
+public static class JsonExtensions
+{
+    public static JsonElement? GetPropertySafe(this JsonElement element, string propertyName) => element.TryGetProperty(propertyName, out JsonElement value) ? value : (JsonElement?)null;
+}
