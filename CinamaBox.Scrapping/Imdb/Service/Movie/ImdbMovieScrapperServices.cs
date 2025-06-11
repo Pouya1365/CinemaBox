70bd016da73b61e79c151d7ac405af5fd3aebd18
Pouya <Pouya@DESKTOP-N60CR32>
2025-06-11 19:42:29 +0330

    ImdbUrlBuilder

---
+++ CinamaBox.Scrapping/Imdb/Service/Movie/ImdbMovieScrapperServices.cs
@@ -0,0 +1,23 @@
+﻿namespace CinamaBox.Scrapping.Imdb.Service.Movie;
+
+public class ImdbMovieScrapperServices
+{
+    public async Task<MovieModel> ScrapeAsync(string imdbId)
+    {
+        var json = await JsonLoader.LoadImdbJsonAsync(imdbId);
+        var movie = new MovieModel();
+
+        movie.Title = TitleExtractor.Extract(json);
+        movie.Runtime = RuntimeExtractor.Extract(json);
+        movie.Countries = CountryExtractor.Extract(json);
+        movie.Credits = CreditExtractor.Extract(json);
+        movie.Locations = LocationExtractor.Extract(json);
+        movie.ProductionCompanies = CompanyExtractor.Extract(json);
+        movie.Taglines = TaglineExtractor.Extract(json);
+        movie.Keywords = KeywordExtractor.Extract(json);
+        movie.Budget = BudgetExtractor.Extract(json);
+        movie.Awards = AwardExtractor.Extract(json);
+
+        return movie;
+    }
+}
---
+++ CinemaBox.Utilities/Imdb/Url/ImdbUrlBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace CinemaBox.Utilities.Imdb.Url;
+
+public static class ImdbUrlBuilder
+{
+    public static string BuildTitleUrl(string imdbId, string? path)
+    {
+        string baseUrl = $"https://m.imdb.com/title/{imdbId}/";
+        return string.IsNullOrEmpty(path) ? baseUrl + "?ref_=tt_rvi_t_2" : baseUrl + path;
+    }
+
+    public static string BuildNameUrl(string imdbId, string? path)
+    {
+        string baseUrl = $"https://m.imdb.com/name/{imdbId}/";
+        return string.IsNullOrEmpty(path) ? baseUrl + "?ref_=tt_rvi_t_3" : baseUrl + path;
+    }
+
+}
