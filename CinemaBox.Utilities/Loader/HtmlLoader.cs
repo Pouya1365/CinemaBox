fd63871afcb14f5f6399a205da4a971c892f94d9
Pouya <Pouya@DESKTOP-N60CR32>
2025-06-11 19:29:41 +0330

    HtmlLoader

---
+++ CinemaBox.Utilities/Loader/HtmlLoader.cs
@@ -0,0 +1,11 @@
+﻿using HtmlAgilityPack;
+
+namespace CinemaBox.Utilities.Loader;
+public static class HtmlLoader
+{
+    public static async Task<HtmlDocument> LoadDocumentAsync(string url)
+    {
+        HtmlWeb web = new();
+        return await web.LoadFromWebAsync(url);
+    }
+}
