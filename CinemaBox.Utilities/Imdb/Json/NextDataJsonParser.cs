dc2b95442f6bd788c781526fa18ad69c6d643372
Pouya <Pouya@DESKTOP-N60CR32>
2025-06-11 19:52:52 +0330

    json

---
+++ CinemaBox.Utilities/Imdb/Json/NextDataJsonParser.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using System.Text.Json;
+
+namespace CinemaBox.Utilities.Imdb.Json;
+
+public static class NextDataJsonParser 
+{
+    public static JsonDocument? Parse(HtmlDocument document)
+    {
+        HtmlNode? nextDataNode = document.DocumentNode.SelectSingleNode("//script[@id='__NEXT_DATA__']");
+        if (nextDataNode == null || string.IsNullOrWhiteSpace(nextDataNode.InnerText))
+            return null;
+        try
+        {
+            return JsonDocument.Parse(nextDataNode.InnerText);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+}
