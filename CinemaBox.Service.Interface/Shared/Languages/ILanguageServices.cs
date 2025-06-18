1ad7340ae958061172967c195be8d95d4ba39bbb
Pouya <Pouya@DESKTOP-SD5KJ4K>
2025-06-18 16:26:23 +0330

    LAnguage Service

---
+++ CinemaBox.Service.Interface/Shared/Languages/ILanguageServices.cs
@@ -0,0 +1,9 @@
+﻿using CinemaBox.Domain.Shared.Languages;
+
+namespace CinemaBox.Service.Interface.Shared.Languages;
+
+public interface ILanguageServices
+{
+    Task<Language?> CreateOrGetLanguageAsync(string? LanguageName, string? isoCode);
+    Task<Language?> GetLanguageAsync(string languageName);
+}
---
+++ CinemaBox.Service/Shared/Languages/LanguageServices.cs
@@ -0,0 +1,31 @@
+﻿using CinemaBox.Domain.Shared.Languages;
+using CinemaBox.Service.Interface.Shared.Languages;
+using CinemaBox.UnitOfWork.Interface.UOW;
+
+namespace CinemaBox.Service.Shared.Languages;
+
+public class LanguageServices(IUnitOfWork unitOfWork) : ILanguageServices
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    public async Task<Language?> CreateOrGetLanguageAsync(string? LanguageName,string? isoCode)
+    {
+        if (string.IsNullOrWhiteSpace(LanguageName))
+            return null;
+        Language? Language = await GetLanguageAsync(languageName: LanguageName);
+        if (Language == null)
+        {
+            Language = new Language { EnLanguageName = LanguageName.Trim() ,IsoCode= isoCode };
+            await _unitOfWork.Repository<Language>().AddAsync(Language);
+            await _unitOfWork.CompleteAsync();
+        }
+        return Language;
+    }
+    public async Task<Language?> GetLanguageAsync(string languageName)
+    {
+        if (string.IsNullOrWhiteSpace(languageName))
+            return null;
+
+        return await _unitOfWork.Repository<Language>()
+            .FindAsync(l => l.EnLanguageName == languageName && l.FaLanguageName==languageName);
+    }
+}
