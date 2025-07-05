5311287218110e7a4474cc970f6edb4a810483e1
Pouya <Pouya@DESKTOP-SD5KJ4K>
2025-07-05 16:12:11 +0330

    keyword

---
+++ CinemaBox.Context/Migrations/20250705124000_InitialDb_V4.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaBox.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDb_V4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EnKeyowrdName",
+                schema: "Shared",
+                table: "Keywords",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                comment: "کلمه کلیدی انگلیسی",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldComment: "کلمه کلیدی انگلیسی");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EnKeyowrdName",
+                schema: "Shared",
+                table: "Keywords",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                comment: "کلمه کلیدی انگلیسی",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldComment: "کلمه کلیدی انگلیسی");
+        }
+    }
+}
