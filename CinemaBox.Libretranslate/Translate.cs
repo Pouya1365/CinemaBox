180035465e67b52e1c680fd255c6cb986b8c5790
PouyaForoozia <PouyaForoozia@outlook.com>
2025-08-23 21:43:35 +0330

    trans

--- CinemaBox.Libretranslate/Translate.cs
+++ CinemaBox.Libretranslate/Translate.cs
@@ -21,29 +21,30 @@ public class Translate : ITranslate
 
             string url = $"https://translate.google.com/m?sl=auto&hl=fa&q={Uri.EscapeDataString(part)}";
 
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
             request.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
             request.Headers.Add("Accept-Language", "fa-IR,fa;q=0.9,en-US;q=0.8,en;q=0.7");
             request.Headers.Referrer = new Uri("https://translate.google.com/");
             request.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-
-            var response = await _client.SendAsync(request);
-            if (!response.IsSuccessStatusCode)
-                return null;
-
-            var content = await response.Content.ReadAsStringAsync();
-
-            var match = Regex.Match(content, @"<div\s+class=""result-container"">(.*?)<\/div>", RegexOptions.Singleline | RegexOptions.IgnoreCase);
-            if (match.Success)
+            try
             {
-                result.Append(System.Net.WebUtility.HtmlDecode(match.Groups[1].Value));
-                result.Append(" ");
+                HttpResponseMessage response = await _client.SendAsync(request);
+                if (!response.IsSuccessStatusCode)
+                    return null;
+                string content = await response.Content.ReadAsStringAsync();
+                Match match = Regex.Match(content, @"<div\s+class=""result-container"">(.*?)<\/div>", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    result.Append(System.Net.WebUtility.HtmlDecode(match.Groups[1].Value));
+                    result.Append(" ");
+                }
             }
+            catch (Exception)
+            {
 
-
-
+             await   TranslateText( text);
+            }                   
         }
-
         return result.ToString().Trim();
     }
 
