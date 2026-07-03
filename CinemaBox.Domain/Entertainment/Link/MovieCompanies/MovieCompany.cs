08b5fcc9da09336e8551699e07eb4673691e5e1c
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 17:05:48 +0330

    MovieCompany Change Type

--- CinemaBox.Domain/Entertainment/Link/MovieCompanies/MovieCompany.cs
+++ CinemaBox.Domain/Entertainment/Link/MovieCompanies/MovieCompany.cs
@@ -5,8 +5,8 @@ namespace CinemaBox.Domain.Entertainment.Link.MovieCompanies;
 
 public class MovieCompany
 {
-    public required string MovieId { get; set; }
-    public required string CompanyId { get; set; }
+    public required int MovieId { get; set; }
+    public required int CompanyId { get; set; }
     public Company? Company { get; set; }
     public Movie? Movie { get; set; }
 }
