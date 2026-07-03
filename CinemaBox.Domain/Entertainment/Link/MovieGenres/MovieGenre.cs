c6df03bf876b7f721c0e2f59395355b905a4779f
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 17:23:13 +0330

    Remove MovieFile And Change Int

--- CinemaBox.Domain/Entertainment/Link/MovieGenres/MovieGenre.cs
+++ CinemaBox.Domain/Entertainment/Link/MovieGenres/MovieGenre.cs
@@ -5,7 +5,7 @@ namespace CinemaBox.Domain.Entertainment.Link.MovieGenres;
 
 public class MovieGenre
 {
-    public required string MovieId { get; set; }
+    public required int MovieId { get; set; }
     public required byte GenreId { get; set; }
     public Genre? Genre { get; set; }
     public Movie? Movie { get; set; }
