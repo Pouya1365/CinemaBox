690dd8cd436323b25b7d1fcc3fdc06edd5550d3a
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 16:51:27 +0330

    Add Some Field

--- CinemaBox.Domain/Entertainment/Coropration/Company.cs
+++ CinemaBox.Domain/Entertainment/Coropration/Company.cs
@@ -1,11 +1,22 @@
-﻿using CinemaBox.Domain.Entertainment.Link.MovieCompanies;
+﻿using CinemaBox.Domain.Division.CountryParts;
+using CinemaBox.Domain.Entertainment.Link.MovieCompanies;
 using CinemaBox.Domain.Persistent;
+using System.Diagnostics.Metrics;
 
 namespace CinemaBox.Domain.Entertainment.Coropration;
 
-public class Company : PersistentObject<string>
+public class Company : PersistentObject<int>
 {
-    public required string EnCompanyName { get; set; }
-    public string? FaCompanyName { get; set; }
+    public required string CompanyName { get; set; }
+    public string? Description { get; set; }
+    public long? LogoPathFileId { get; set; }  // "/logos/warner-bros.png"
+    public string? Headquarters { get; set; }  // "Burbank, California, USA"
+    public string? Website { get; set; }
+    // External IDs
+    public int? TmdbId { get; set; }
+    public string? ImdbId { get; set; }  // "co0002663"
+    public long? CountryId{ get; set; }  // ISO 3166-1: "US", "IR"
+    public int? FoundedYear { get; set; }
     public ICollection<MovieCompany> MovieCompanies { get; set; } = [];
+    public CountryPart CountryPart { get; set; }
 }
