9721bf841f2abf43ecd8254759502f17ed51eab5
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 18:37:59 +0330

    Add Field

--- CinemaBox.Domain/Files/Files/File.cs
+++ CinemaBox.Domain/Files/Files/File.cs
@@ -1,19 +1,18 @@
-﻿using CinemaBox.Domain.Entertainment.Genres;
-using CinemaBox.Domain.Entertainment.Link.MovieFiles;
-using CinemaBox.Domain.Managment.Link.UserMovieFiles;
-using CinemaBox.Domain.Persistent;
-using CinemaBox.Domain.Person.PeopleFiles;
+﻿using CinemaBox.Domain.Persistent;
 using CinemaBox.Domain.Servers.Servers;
 namespace CinemaBox.Domain.Files.Files;
 
 public class File : PersistentObject<long>
 {
     public required byte ServerId { get; set; }
-
     public string? FileName { get; set; }
+    public long FileSize { get; set; }
+    public required string MimeType { get; set; }
+    public string? FileExtension { get; set; }
+    public string? OriginalFileName { get; set; }
+    // ===== Media Specific (optional) =====
+    public int? Width { get; set; }
+    public int? Height { get; set; }
+    public int? DurationSeconds { get; set; }
     public Server? Server { get; set; }
-    public ICollection<PeopleFile> PeopleFiles { get; set; } = [];
-    public ICollection<MovieFile> MovieFiles { get; set; } = [];
-    public ICollection<Genre> Genres { get; set; } = [];
-    public ICollection<UserMovieFile> UserMovieFile { get; set; } = [];
 }
