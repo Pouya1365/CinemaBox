f4aa726c71c005ac7b124dc8dc01b00d908f224d
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 18:41:47 +0330

    Add User

--- CinemaBox.Domain/Managment/Link/UserMovieAudios/UserMovieAudio.cs
+++ CinemaBox.Domain/Managment/Link/UserMovieAudios/UserMovieAudio.cs
@@ -2,19 +2,19 @@
 using CinemaBox.Domain.Persistent;
 using CinemaBox.Domain.Shared.Formats;
 using CinemaBox.Domain.Shared.Languages;
-
+using CinemaBox.Domain.UserManager.Users;
 namespace CinemaBox.Domain.Managment.Link.UserMovieAudios;
-
 public class UserMovieAudio:PersistentObject<int>
 {
-
+    public byte? UserId { get; set; }
     public byte? LanguageId { get; set; }
-    public string? MovieId { get; set; }
+    public int? MovieId { get; set; }
     public byte? Channels { get; set; }
     public byte? FormatId { get; set; }
+    public int? Bitrate { get; set; }
+    public int? SampleRate { get; set; }
     public Language? Language { get; set; }
     public Format? Format { get; set; }
     public Movie? Movie { get; set; }
-
-
+    public User? User { get; set; }
 }
