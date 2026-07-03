7a16ee75f90ae07b9227f55dcb936e8c2ab975a5
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 16:54:11 +0330

    Change Name

--- CinemaBox.Domain/Entertainment/CreditTypes/CreditType.cs
+++ CinemaBox.Domain/Entertainment/CreditTypes/CreditType.cs
@@ -1,11 +1,9 @@
 ﻿using CinemaBox.Domain.Entertainment.Link.MovieCredits;
 using CinemaBox.Domain.Persistent;
-
 namespace CinemaBox.Domain.Entertainment.CreditTypes;
-
 public class CreditType : PersistentObject<byte>
 {
-    public required string EnCreditTypeName { get; set; }
-    public string? FaCreditTypeName { get; set; }
+    public required string CreditTypeName { get; set; }
+    public string? PersianCreditTypeName { get; set; }
     public ICollection<MovieCredit> MovieCredits { get; set; } = [];
 }
