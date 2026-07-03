5d7f43555c73d6ad4cad15b3ee988ac77b99684a
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 17:21:52 +0330

    Add Field

--- CinemaBox.Domain/Entertainment/Link/MovieCredits/MovieCredit.cs
+++ CinemaBox.Domain/Entertainment/Link/MovieCredits/MovieCredit.cs
@@ -6,11 +6,14 @@ namespace CinemaBox.Domain.Entertainment.Link.MovieCredits;
 
 public class MovieCredit
 {
-    public required string MovieId { get; set; }
-    public required string PeopleId { get; set; }
+    public required int MovieId { get; set; }
+    public required int PeopleId { get; set; }
     public required byte CreditTypeId { get; set; }
     public bool? IsLead { get; set; }
-    public string? RoleName { get; set; }
+    public string? CharacterName { get; set; }
+    public string? PersianCharacterName { get; set; }
+    public int? TmdbCreditId { get; set; }
+    public string? ImdbCreditId { get; set; }
     public Movie? Movie { get; set; }
     public People? People { get; set; }
     public CreditType? CreditType { get; set; }
