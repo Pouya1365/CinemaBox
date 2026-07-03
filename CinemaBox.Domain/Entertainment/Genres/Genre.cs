bf0227c4ce7355e83da805a0adb0c8bf0201eeb6
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 17:01:36 +0330

    Add Field

--- CinemaBox.Domain/Entertainment/Genres/Genre.cs
+++ CinemaBox.Domain/Entertainment/Genres/Genre.cs
@@ -5,9 +5,11 @@ namespace CinemaBox.Domain.Entertainment.Genres;
 
 public class Genre : PersistentObject<byte>
 {
-    public required string EnGenreName { get; set; }
-    public string? FaGenreName { get; set; }
+    public required string GenreName { get; set; }
+    public string? PersianGenreName { get; set; }
     public long? FileId { get; set; }
+    public int? TmdbId { get; set; }
+    public string? ImdbId { get; set; }
     public Files.Files.File? File { get; set; }
     public ICollection<MovieGenre> MovieGenres { get; set; } = [];
 }
