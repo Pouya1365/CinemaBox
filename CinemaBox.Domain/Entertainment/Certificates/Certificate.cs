fa71601adb0fdb393e3aac9eff442755930bf042
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 15:52:14 +0330

    Remover Space Line

--- CinemaBox.Domain/Entertainment/Certificates/Certificate.cs
+++ CinemaBox.Domain/Entertainment/Certificates/Certificate.cs
@@ -1,8 +1,6 @@
 ﻿using CinemaBox.Domain.Entertainment.Movies;
 using CinemaBox.Domain.Persistent;
-
 namespace CinemaBox.Domain.Entertainment.Certificates;
-
 public class Certificate : PersistentObject<byte>
 {
     public required string CertificateName { get; set; }
