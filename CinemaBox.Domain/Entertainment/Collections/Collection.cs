dfed655e12da48aaa2615b3cffe6832aad7f1670
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 16:44:01 +0330

    Add Collection Poster

--- CinemaBox.Domain/Entertainment/Collections/Collection.cs
+++ CinemaBox.Domain/Entertainment/Collections/Collection.cs
@@ -5,9 +5,11 @@ namespace CinemaBox.Domain.Entertainment.Collections;
 
 public class Collection : PersistentObject<int>
 {
-    public required string EnCollectionName { get; set; }
-    public string? FaCollectionName { get; set; }
-    public int? TotalCount { get; set; }
-    public int? CountCollection { get; set; }
+    public required string CollectionName { get; set; }
+    public string? PersianCollectionName { get; set; }
+    public int? TotalMoviesInCollection { get; set; }
+    public string? Description { get; set; }           // e.g. "The story of..."
+    public long? PosterPathFileId { get; set; }            // Collection poster
+    public long? BackdropPathFileId { get; set; }          // Collection backdrop
     public ICollection<Movie> Movies { get; set; } = [];
 }
