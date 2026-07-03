04f83de71880a6c2af82999f6c0111e838227521
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 17:19:47 +0330

    Change Int

--- CinemaBox.Domain/Entertainment/Link/MovieCountries/MovieCountry.cs
+++ CinemaBox.Domain/Entertainment/Link/MovieCountries/MovieCountry.cs
@@ -5,8 +5,8 @@ namespace CinemaBox.Domain.Entertainment.Link.MovieCountries;
 
 public class MovieCountry
 {
-    public required string MovieId { get; set; }
-    public required long CountryPartId { get; set; }
+    public required int MovieId { get; set; }
+    public required int CountryPartId { get; set; }
     public CountryPart? CountryPart { get; set; }
     public Movie? Movie { get; set; }
 }
