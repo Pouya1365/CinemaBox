3f0f4a647d415f6cd5892188154e0831ef5d8213
Pouya <Pouya@DESKTOP-0VD3A5I>
2026-07-03 17:55:52 +0330

    Movie Add Field

--- CinemaBox.Domain/Entertainment/Movies/Movie.cs
+++ CinemaBox.Domain/Entertainment/Movies/Movie.cs
@@ -3,126 +3,58 @@ using CinemaBox.Domain.Entertainment.Collections;
 using CinemaBox.Domain.Entertainment.Link.MovieCompanies;
 using CinemaBox.Domain.Entertainment.Link.MovieCountries;
 using CinemaBox.Domain.Entertainment.Link.MovieCredits;
-using CinemaBox.Domain.Entertainment.Link.MovieFiles;
 using CinemaBox.Domain.Entertainment.Link.MovieGenres;
 using CinemaBox.Domain.Entertainment.Link.MovieKeywords;
 using CinemaBox.Domain.Entertainment.Link.MovieLocations;
-using CinemaBox.Domain.Entertainment.Link.MovieSpokenLanguages;
 using CinemaBox.Domain.Entertainment.Link.MovieTaglines;
-using CinemaBox.Domain.Managment.Link.UserMovieAudios;
-using CinemaBox.Domain.Managment.Link.UserMovieDisks;
-using CinemaBox.Domain.Managment.Link.UserMovieFiles;
-using CinemaBox.Domain.Managment.Link.UserMovieVideos;
 using CinemaBox.Domain.Persistent;
 using CinemaBox.Domain.Shared.Currencies;
-
 namespace CinemaBox.Domain.Entertainment.Movies;
-public class Movie : PersistentObject<string>
+public class Movie : PersistentObject<int>
 {
-    /// <summary>
-    /// عنوان انگلیسی
-    /// </summary>
-    public string? EnTitle { get; set; }
-    /// <summary>
-    /// عنوان فارسی
-    /// </summary>
-    public string? FaTitle { get; set; }
-    /// <summary>
-    /// عنوان اصلی
-    /// </summary>
+    public string? ImdbId { get; set; }
+    public string? TmdbId { get; set; }
+    public string? Title { get; set; }
+    public string? PersianTitle { get; set; }
     public string? OriginalTitle { get; set; }
-    /// <summary>
-    /// سال فیلم/تاریخ شروع سریال
-    /// </summary>
-    public long? StartYear { get; set; }
-    /// <summary>
-    /// تاریخ پایان سریال
-    /// </summary>
-    public long? EndYear { get; set; }
-    /// <summary>
-    /// شناسه درجه سنی
-    /// </summary>
+    public int? StartYear { get; set; }
+    public int? EndYear { get; set; }
+    public int? ReleaseYear { get; set; }
+    public byte? ReleaseMonth { get; set; }
+    public byte? ReleaseDay { get; set; }
+    public int? RunTimeMinutes { get; set; }
     public byte? CertificateId { get; set; }
-    /// <summary>
-    /// مدت زمان فیلم دقیقه
-    /// </summary>
-    public long? RunTimeMinutes { get; set; }
-    /// <summary>
-    /// سال انتشار
-    /// </summary>
-    public long? ReleaseYear { get; set; }
-    /// <summary>
-    /// ماه انتشار
-    /// </summary>
-    public long? ReleaseMonth { get; set; }
-    /// <summary>
-    /// روز انتشار
-    /// </summary>
-    public long? ReleaseDay { get; set; }
-    /// <summary>
-    /// رتبه بندی کل
-    /// </summary>
+    public int? TotalSeasons { get; set; }
+    public int? TotalEpisodes { get; set; }
     public decimal? AggregateRating { get; set; }
-    /// <summary>
-    /// تعداد رای دهندگان
-    /// </summary>
     public long? VoteCount { get; set; }
-    /// <summary>
-    /// تعداد جوایز
-    /// </summary>
-    public long? Winner { get; set; }
-    /// <summary>
-    /// نامزد شدن
-    /// </summary>
-    public long? Nomination { get; set; }
-    /// <summary>
-    /// خلاصه داستان انگلیسی
-    /// </summary>
-    public string? EnPlot { get; set; }
-    /// <summary>
-    ///  داستان انگلیسی
-    /// </summary>
-    public string? EnStoryline { get; set; }
-    /// <summary>
-    /// بودجه
-    /// </summary>
-    public double? Budget { get; set; }
-    /// <summary>
-    ///  داستان فارسی
-    /// </summary>
-    public string? FaStoryline { get; set; }
-    /// <summary>
-    /// برترین فیلم
-    /// </summary>
+    public int? Winner { get; set; }
+    public int? Nomination { get; set; }
+    public string? Plot { get; set; }
+    public string? Storyline { get; set; }
+    public decimal? Budget { get; set; }
+    public string? PersianStoryline { get; set; }
     public int? TopRank { get; set; }
-    /// <summary>
-    /// تعداد برنده اسکار
-    /// </summary>
     public byte? OscarWins { get; set; }
-    /// <summary>
-    /// تعداد نامزد اسکار
-    /// </summary>
     public byte? OscarNominations { get; set; }
-    /// <summary>
-    /// شناسه واحد پول
-    /// </summary>
+    public double? Popularity { get; set; }
+    public decimal? Revenue { get; set; }
+    public byte? RevenueCurrencyId { get; set; }
+    public string? TrailerUrl { get; set; }
+    public long? PosterPathFileId { get; set; }
+    public long? BackdropPathFileId { get; set; }
     public byte? BudgetCurrencyId { get; set; }
     public bool? IsTvShow { get; set; }
     public int? CollectionId { get; set; }
     public Certificate? Certificate { get; set; }
-    public Currency? Currency { get; set; }
+    public Currency? RevenueCurrency { get; set; }
+    public Currency? BudgetCurrency { get; set; }
     public Collection? Collection { get; set; }
-    public ICollection<MovieCompany> MovieCompanies { get; set; } = [];
-    public ICollection<MovieCountry> MovieCountries { get; set; } = [];
-    public ICollection<MovieCredit> MovieCredits { get; set; } = [];
-    public ICollection<MovieFile> MovieFiles { get; set; } = [];   
-    public ICollection<MovieGenre> MovieGenres { get; set; } = [];
-    public ICollection<MovieKeyword> MovieKeywords { get; set; } = [];
-    public ICollection<MovieLocation> MovieLocations { get; set; } = [];
-    public ICollection<MovieSpokenLanguage> MovieSpokenLanguages { get; set; } = [];
-    public ICollection<MovieTagline> MovieTaglines { get; set; } = [];
-    public ICollection<UserMovieDisk> UserMovieDisks { get; set; } = [];
-    public ICollection<UserMovieAudio> UserMovieAudios { get; set; } = [];
-    public ICollection<UserMovieVideo> UserMovieVideos { get; set; } = [];
-    public ICollection<UserMovieFile> UserMovieFile { get; set; } = [];
+    public ICollection<MovieCompany> movieCompanies { get; set; } = [];
+    public ICollection<MovieCountry> movieCountries { get; set; } = [];
+    public ICollection<MovieCredit> movieCredits { get; set; } = [];
+    public ICollection<MovieGenre> movieGenres { get; set; } = [];
+    public ICollection<MovieKeyword> movieKeywords { get; set; } = [];
+    public ICollection<MovieLocation> movieLocations { get; set; } = [];
+    public ICollection<MovieTagline> movieTaglines { get; set; } = [];
 }
